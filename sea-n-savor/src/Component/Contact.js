5488fb326da053ef492c34aa82f38c8a530f5bee
Janet Njeri Ngungu <janetnjerin89@gmail.com>
2022-07-31 02:38:45 +0300

    add contact  style

--- sea-n-savor/src/Component/Contact.js
+++ sea-n-savor/src/Component/Contact.js
@@ -7,22 +7,27 @@ function handleSubmit(){
 }
 
     return(
+<div className="contact-form">
         <form className="form" onSubmit = {handleSubmit} >
-            <h2>Talk to us! We will be glad to here your views!</h2>
-            <div>
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" required />
+            <h4>Talk to us!</h4>
+            <div className="flex-form">
+            <div className="input">
+                {/* <label htmlFor="name">Name:</label> */}
+                <input placeholder="Enter Your Name" type="text" id="name" required />
             </div>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" required />
+            <div className="input">
+                {/* <label htmlFor="email">Email:</label> */}
+                <input placeholder=" Enter Your Email" type="email" id="email" required />
             </div>
-            <div>
-                <label htmlFor="message">Message:</label>
-                <textarea id="message" required />
+            <div className="input text">
+                {/* <label htmlFor="message">Message:</label> */}
+
+                <textarea placeholder="Enter your message here" id="message" required />
             </div>
             <button type="submit">Submit</button>
+            </div>
         </form>
+        </div>
     )
 }
 
